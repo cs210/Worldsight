a06917a48f2715ec60041c12b20941d310ae2e47
Aakanksha Saxena <asaxena1@stanford.edu>
2020-04-28 13:21:55 -0700

    Email integration

--- meshworks/client/src/components/EmailSenderTest.js
+++ meshworks/client/src/components/EmailSenderTest.js
@@ -6,7 +6,19 @@ class EmailSenderTest extends Component {
 
 
   sendEmail = () => {
-    // Do something
+      // var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
+
+      // Email.send({
+      // Host: "smtp.gmail.com",
+      // Username : "worldsightvr@gmail.com",
+      // Password : "worldsightcs210",
+      // To : 'aakanksha.j.saxena@gmail.com',
+      // From : "worldsightvr@gmail.com",
+      // Subject : "Testing",
+      // Body : "Testin",
+      // }).then(
+      //   message => alert("mail sent successfully")
+      // );
   }
 
   render() {
