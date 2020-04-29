377bf372508c78dafc5ebc3a8e78e9f4782d4235
Grace Cheng <gcheng21@stanford.edu>
2020-04-29 16:15:57 -0700

    fixed uploader

--- meshworks/client/src/components/Uploader.js
+++ meshworks/client/src/components/Uploader.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Header, Grid, Button } from "semantic-ui-react";
+import { Header, Grid } from "semantic-ui-react";
 import DropzoneS3Uploader from 'react-dropzone-s3-uploader';
 require('dotenv').config();
 
@@ -30,68 +30,19 @@ class Uploader extends Component {
     super(props);
   }
 
-  changeHandle = (event) => {
-    const inputValue = event.target.value;
-    const propertyKey = event.target.getAttribute('name');
-    console.log('propertyKey', propertyKey);
-    console.log('inputValue', inputValue);
-    this.setState({
-      inventoryEntry: {
-        ...this.state.inventoryEntry,
-        [propertyKey]: inputValue,
-      }
-    });
-  }
-
   state = {
     success: false,
     url: "",
     error: false,
     errorMessage: "",
   };
-
-  handleChange = (event) => {
-    this.setState({ success: false, url: "" });
-  };
-
+  
   handleFinishedUpload = info => {
     console.log('File uploaded with filename', info.filename)
     console.log('Access it on s3 at', info.fileUrl)
   }
 
-  sendMessage(message, image) {
-    const newMessage = {
-      type: 'image',
-      image: image,
-    };
-  }
-
   render() {
-    // const SuccessMessage = () => (
-    //   <div style={{ padding: 50 }}>
-    //     <Header size="medium" color="green">
-    //       {" "}
-    //       Upload Successful{" "}
-    //     </Header>
-    //     <img
-    //       src={this.state.url}
-    //       width="300"
-    //       alt="Successfully uploaded image"
-    //     ></img>
-    //     <br />
-    //   </div>
-    // );
-    // const ErrorMessage = () => (
-    //   <div style={{ padding: 50 }}>
-    //     <Header as="h1" color="red">
-    //       {" "}
-    //       Upload failure{" "}
-    //     </Header>
-    //     <span style={{ color: "red", backgroundColor: "black" }}>ERROR: </span>
-    //     <span>{this.state.errorMessage}</span>
-    //     <br />
-    //   </div>
-    // );
     const style = {
       height: 200,
       border: 'dashed 2px #999',
@@ -115,7 +66,7 @@ class Uploader extends Component {
         >
           <Grid.Column style={{ maxWidth: 450 }}>
             <Header size="huge"> UPLOAD A FILE </Header>
-            <DropzoneS3Uploader onFinish={this.handleFinishedUpload} {...uploadOptions} />
+            <DropzoneS3Uploader onFinish={this.handleFinishedUpload} {...uploadOptions}><UploadDisplay /></DropzoneS3Uploader>
           </Grid.Column>
         </Grid>
       </div>
