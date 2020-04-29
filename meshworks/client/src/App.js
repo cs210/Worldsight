1196e422da9d22f86c905ecc96f6b03f04d3dfa0
Jihee Hwang <alarmringing@gmail.com>
2020-04-28 17:30:20 -0700

    Styling updates

--- meshworks/client/src/App.js
+++ meshworks/client/src/App.js
@@ -6,6 +6,7 @@ import 'semantic-ui-less/semantic.less'
 import Navigator from './components/Navigator';
 import Tester from './components/Tester';
 import Uploader from './components/Uploader';
+import ThemingLayout from './components/ThemingLayout';
 
 
 class App extends Component {
@@ -15,6 +16,7 @@ class App extends Component {
       <Router>
         <Route path='/' exact component={Navigator} />
         <Route path='/tests' component={Tester} />
+        <Route path='/theming' component={ThemingLayout} />
         <Route path='/upload' component={Uploader} />
       </Router>
     );
--- meshworks/client/src/components/Navigator.js
+++ meshworks/client/src/components/Navigator.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Uploader from './Uploader'
-import {Menu, Header, Container, Button, Search} from 'semantic-ui-react'
+import {Menu, Header, Container, Button, Search, Icon} from 'semantic-ui-react'
 import {Link} from "react-router-dom"
 
 const Navigator = () => {
@@ -8,14 +8,20 @@ const Navigator = () => {
     <div>
       <Menu borderless fixed='top' inverted>
         <Menu.Item>
-          <Header size='huge' color='blue'> Meshworks </Header>
+          <Header primary weight='900'> Meshworks </Header>
         </Menu.Item>
         <Menu.Menu position='right'>
             <Menu.Item>
-              <Button primary as={Link} to="/upload">Upload</Button>
+              <Button primary animated='vertical' as={Link} to="/upload">
+                <Button.Content visible> UPLOAD </Button.Content>
+                <Button.Content hidden> <Icon name='arrow alternate circle up outline'/> </Button.Content>
+              </Button>
             </Menu.Item>
             <Menu.Item>
-              <Button secondary as={Link} to="/tests">Testpage</Button>
+              <Button as={Link} to="/tests">Testpage</Button>
+            </Menu.Item>
+            <Menu.Item>
+              <Button as={Link} to="/theming">Theme Review</Button>
             </Menu.Item>
             <Menu.Item>
               <Search />
