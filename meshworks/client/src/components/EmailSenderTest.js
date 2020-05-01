1f8ffc11bfa8d367a678172557478db3b553d61e
Jihee Hwang <alarmringing@gmail.com>
2020-05-01 01:31:54 -0700

    Email works now

--- meshworks/client/src/components/EmailSenderTest.js
+++ meshworks/client/src/components/EmailSenderTest.js
@@ -3,23 +3,38 @@ import React, {Component} from 'react';
 import {Header, Input} from 'semantic-ui-react';
 import emailjs from 'emailjs-com';
 
+const service_id = "gmail";
+const user_id = "user_GPvH16KTRm7olTHDX1cdy"
+const template_id = "template_jPWa5xhn";
+
 class EmailSenderTest extends Component {
 
-  render() {
+  state = {
+    email: ""
+  }
 
-    var service_id = "gmail";
-    var user_id = "user_GPvH16KTRm7olTHDX1cdy"
-    var template_id = "template_jPWa5xhn";
+  handleChange = (e) => {
+    this.setState({
+      email: e.target.value
+    });
+  }
+
+  sendEmail = () => {
     var template_params = {
-     "reply_to": "worldsightvr@gmail.com",
+     "reply_to": this.state.email,
     }
+    emailjs.send(service_id, template_id, template_params, user_id)
+  }
+
+  render() {
 
     return (
       <div>
       <Header as='h1'> Send Email Test </Header>
       <Input
+        onChange={this.handleChange}
         action={{
-          onClick: emailjs.send(service_id, template_id, template_params, user_id),
+          onClick: this.sendEmail,
           color: 'primary',
           actionPosition: 'right',
           content: 'Send Email',
