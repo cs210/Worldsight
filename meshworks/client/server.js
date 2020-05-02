f877ae39c9f156258869134f1efa1916aa172ccd
Grace Cheng <gcheng21@stanford.edu>
2020-05-02 10:25:06 -0700

    fixing server thing

--- meshworks/client/server.js
+++ meshworks/client/server.js
@@ -13,12 +13,12 @@ app.get('/', function (req, res) {
 });
 
 app.use('/s3', require('react-dropzone-s3-uploader/s3router')({
-	bucket: process.env.AWS_BUCKET_NAME,
+	S3_BUCKET: process.env.AWS_BUCKET_NAME,
 	region: process.env.AWS_REGION,
 	headers: {'Access-Control-Allow-Origin': '*'},
 	ACL: 'public-read',
   }));
-  
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
 	console.log(`Server running on port ${port}`)
