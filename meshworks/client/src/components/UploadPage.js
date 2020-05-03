4066a502b5f91c7722f2488e4d7ed6467a3ea643
Jihee Hwang <alarmringing@gmail.com>
2020-05-03 10:01:26 -0700

    Multi-upload upon button finally working

--- meshworks/client/src/components/UploadPage.js
+++ meshworks/client/src/components/UploadPage.js
@@ -1,18 +1,16 @@
 import React, {Component} from 'react';
-import {Container, Grid, Header, Input, Button} from 'semantic-ui-react';
+import {Container, Message, Grid, Header, Input, Button} from 'semantic-ui-react';
 import emailjs from 'emailjs-com';
-import S3Upload from 'react-s3-uploader/s3upload'
-
+import S3Upload from 'react-s3-uploader/s3upload.js'
 
 import FileUploader from './FileUploader'
-import Uploader from './Uploader'
-
 
 //params for uploading to S3.
 const S3_BUCKET_URL= 'https://meshworks.s3.amazonaws.com/'
 const uploadOptions = {
   server: 'http://localhost:5000',
-  s3Url: S3_BUCKET_URL
+  s3path: S3_BUCKET_URL,
+  signingUrl: "/s3/sign"
 }
 
 // params for sending out email.
@@ -20,12 +18,16 @@ const service_id = "gmail";
 const user_id = "user_GPvH16KTRm7olTHDX1cdy"
 const template_id = "template_jPWa5xhn";
 
-
 class UploadPage extends Component {
 
   state = {
     email: "",
-    filesReady: []
+    filesReady: [],
+    submitCompleteMessage: false
+  }
+
+  handleSubmitCompleteMessageDismiss = () => {
+    this.setState({submitCompleteMessage: false});
   }
 
   handleChange = (e) => {
@@ -34,10 +36,37 @@ class UploadPage extends Component {
     });
   }
 
+  // This comes from the dropbox implementation.
   handleFileChangeStatus = (file, status, allFiles) => {
     if (status == 'done') {
-      this.state.filesReady.push(file.file)
+      this.state.filesReady.push(file)
     }
+  }
+
+  onProgress = (percent, status, file) => {
+    console.log("Progress percent: " + percent)
+  }
+
+  onError = (status, file) => {
+    console.log("Error uploading: " + status)
+  }
+
+  onFinishS3Put = (signResult, file) => {
+    console.log(signResult, file)
+    this.setState({submitCompleteMessage: true});
+    // If all files are done uploading
+  }
+
+  uploadFiles = () => {
+    var filesArray = this.state.filesReady.map(function(item){ return item.file;});
+    const options = {
+      files: filesArray,
+      onFinishS3Put: this.onFinishS3Put,
+      onError: this.onError,
+      onProgress: this.onProgress,
+      ...uploadOptions,
+    }
+    new S3Upload(options);
   }
 
   sendEmail = () => {
@@ -47,36 +76,34 @@ class UploadPage extends Component {
     emailjs.send(service_id, template_id, template_params, user_id)
   }
 
-  uploadFiles = () => {
-    console.log(this.state.filesReady);
-
-    const options = {
-      ...this.state.filesReady,
-      ...uploadOptions,
-    }
-    new S3Upload(options);
-  }
-
-  upload = () => {
+  submitEverything = () => {
     this.uploadFiles();
     this.sendEmail();
   }
 
   render() {
+    var submitCompleteMessage =
+      <Message floating
+        onDismiss={this.handleSubmitCompleteMessageDismiss}
+        header='Upload complete!'
+        content='Your files have been submitted for 3d magic! Check your inbox in a few minutes.'
+      />
+
     return (
       <Grid textAlign="center">
         <Grid.Column style={{width: '60%'}}>
             <Header as='h1'> Create your mesh today! </Header>
             <FileUploader
               onChangeStatus={this.handleFileChangeStatus}/>
-            <Uploader />
             <Header as='h3'> Where should we send this to later? </Header>
             <Input
               onChange={this.handleChange}
-              defaultValue='worldsightvr@gmail.com'
+              defaultValue='youremail@example.com'
             />
             <Container style={{marginTop:'5em'}}>
-              <Button primary onClick={this.upload}> UPLOAD! </Button>
+              <Button primary onClick={this.submitEverything}> UPLOAD! </Button>
+              {this.state.submitCompleteMessage ? submitCompleteMessage : null}
+
             </Container>
           </Grid.Column>
       </Grid>
