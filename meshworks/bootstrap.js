851530710523532fe878a048157895329b805e73
Jihee Hwang <alarmringing@gmail.com>
2020-05-15 07:43:22 -0700

    Tried using a are-you-es5 workaround, still runs into web components not being available in the server issues

--- meshworks/bootstrap.js
+++ meshworks/bootstrap.js
@@ -3,15 +3,24 @@
 // We’ll actually use this as an entry point.
 
 require ('ignore-styles');
+const path = require("path");
+const fs = require("fs");
 
 //require('@babel/polyfill')
 require('@babel/register')({
-  ignore: [/(node_modules)/],
+  root: __dirname,
+  //ignore: [/(node_modules)/],
+  ignore: [new RegExp(
+          fs
+            .readFileSync(path.resolve('./non_ES5_node_modules'), 'utf-8')
+            .slice(1, -2)
+        )],
   //ignore: [],
-  //include: ['index.js','server-side-renderer.js', /.\/client\/src/],
   presets: ['@babel/preset-env', '@babel/preset-react'],
   plugins: ['@babel/plugin-syntax-dynamic-import',
-          '@babel/plugin-transform-modules-commonjs']
+          '@babel/plugin-transform-modules-commonjs',
+           '@babel/plugin-transform-runtime',
+          ]
 });
 
 
