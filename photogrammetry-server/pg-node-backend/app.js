37b57c0d3bd61b40f0ee2f00d87d26ef82d8c98f
Jihee Hwang <alarmringing@gmail.com>
2020-05-25 05:51:34 -0700

    db watching in app.js

--- photogrammetry-server/pg-node-backend/app.js
+++ photogrammetry-server/pg-node-backend/app.js
@@ -1,4 +1,31 @@
 const http = require('http');
+const mongoose = require('mongoose');
+const Item = require('./models/item.js');
+
+// Use environment variables with dotenv
+require('dotenv').config();
+
+// Connect to mongoose
+mongoose.connect(process.env.DB, {useNewUrlParser: true})
+  .then(() => console.log(`Database connected successfully`))
+  .catch(err => console.log(err));
+
+Item.watch().on('change', async (data) => {
+  const newItem = data.fullDocument;
+  console.log("New item: ", newItem);
+
+  // Check item is valid (includes photoURLs, etc)
+  // create directory in pg_inputs named the newItem._id
+  // download the images from newItem.photoURLs
+  // RUN PG. convert to mesh (output in pg_outputs)
+  // upload mesh to S3, then update the meshURL field of this item in db
+  // send completion email with mesh-works.io/mesh/{newItem._id} as link
+  // delete downloaded directory in pg_inputs
+});
+
+
+
+
 
 // Create an instance of the http server to handle HTTP requests
 let app = http.createServer((req, res) => {
@@ -8,5 +35,5 @@ let app = http.createServer((req, res) => {
   res.end('Hello World!\n');
 });
 
-app.listen(3000, '127.0.0.1');
-console.log('Node server running on port 3000');
+app.listen(8080, '127.0.0.1');
+console.log('Node server running on port 8080');
