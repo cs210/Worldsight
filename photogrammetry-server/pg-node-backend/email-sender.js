fca5c7d556f849e70679886f84d2c27a15d4aa3d
farhanazam98 <farhanazam98@yahoo.com>
2020-06-01 23:57:34 +0000

    added email functionality

--- photogrammetry-server/pg-node-backend/email-sender.js
+++ photogrammetry-server/pg-node-backend/email-sender.js
@@ -1,18 +1,52 @@
+require('dotenv').config()
+var nodemailer = require('nodemailer');
 const MESH_PAGE_URL = "http://mesh-works.io/mesh/"
 
-async function sendConfirmationEmail(email, itemName) {
-  // Send email to email address,
-  // saying that the {itemName} mesh have been uploaded to server
-  // for conversion.
+var transport = nodemailer.createTransport({
+    server: 'gmail',
+    host: 'smtp.gmail.com',
+    auth: {
+	user: 'worldsightvr@gmail.com',
+	pass: process.env.GMAIL_PW
+    }
+});
 
+
+async function sendConfirmationEmail(email, itemName) {
+    // Send email to email address,
+    // saying that the {itemName} mesh have been uploaded to server
+    // for conversion.
+    var mailOptions = {
+	from: 'worldsightvr@gmail.com',
+	to: email,
+	subject: 'Your item is being processed',
+	html:"<p>" + itemName + " is being uploaded to server for conversion </p>"
+    };
+    transport.sendMail(mailOptions, (error, info) => {
+	if (error) {
+	    return console.log(error);
+	}
+	console.log('Email sent: ' + info.response);
+    });
   return; // TODO: Implement.
 }
 
 async function sendMeshCompleteEmail(email, itemID, itemName) {
-  // Send email to email address,
-  // saying that the {itemName} mesh hae been meshified and
-  // viewable at the link: http://mesh-works.io/mesh/{itemID}
-
+    // Send email to email address,
+    // saying that the {itemName} mesh hae been meshified and
+    // viewable at the link: http://mesh-works.io/mesh/{itemID}
+    var mailOptions = {
+        from: 'worldsightvr@gmail.com',
+        to: email,
+        subject: 'Your mesh is ready!',
+        html: "<div><p>" + itemName + " has been meshified and is viewable at the link: </p><a href=" + MESH_PAGE_URL + itemID + ">Your mesh</a></div>"               
+    };
+    transport.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Email sent: ' + info.response);
+    });
   return; // TODO: Implement.
 }
 
@@ -20,7 +54,3 @@ module.exports = {
   sendConfirmationEmail: sendConfirmationEmail,
   sendMeshCompleteEmail: sendMeshCompleteEmail
 };
-
-// TODO:
-// Unblock to test functionality via: node email-sender.js
-// sendConfirmationEmail('worldsightvr@gmail.com', 'Test Mesh')
