e79a8b61ccbfc792806f04820619c59eb8a12586
Aakanksha Saxena <asaxena1@stanford.edu>
2020-06-05 10:51:18 -0700

    Added infinite scrolling to mesh feed

--- meshworks/frontend/pages/index.js
+++ meshworks/frontend/pages/index.js
@@ -3,6 +3,7 @@ import {Container,Grid,Image,Header} from 'semantic-ui-react'
 import axios from 'axios';
 import dynamic from 'next/dynamic';
 import Link from 'next/link';
+import InfiniteScroll from "react-infinite-scroll-component";
 const DynamicModelViewer = dynamic(
   () => import('../components/ModelViewer.js'),
   { ssr: false }
@@ -12,30 +13,41 @@ class Feed extends Component {
 
 	constructor(props){
 		super(props);
-		this.state = {items: [], defaultMesh: "https://meshworks.s3.amazonaws.com/glb-files/out.glb", customWidth:"50%",customHeight:"20em"};
+		this.state = {items: [], defaultMesh: "https://meshworks.s3.amazonaws.com/glb-files/out.glb", customWidth:"50%",customHeight:"20em",showItems:6};
+		this.handleShowMore = this.handleShowMore.bind(this);
+		this.getItems();
 	}
 
 	componentDidMount(){
 		this.getItems();
 	}
 
+	handleShowMore() {
+		this.setState({showItems: this.state.showItems + 6 >= this.state.items.length ? this.state.showItems + 1 : this.state.showItems + 6});
+  }
+
 	getItems = () => {
 	    axios.get('/api/items')
 	      .then(res => {
 	        if(res.data){
-	          console.log(res.data);
-	          this.setState({
-	            items: res.data
+	          // console.log(res.data);
+	          this.setState({items: res.data},() =>{
+	          	if(this.state.items.length < this.state.showItems){
+	     			this.setState({showItems:this.state.items.length-1});
+	     		}
 	          })
 	        }
 	      })
-	      .catch(err => console.log(err))
+	      .catch(err => console.log(err));
+
+	     // console.log("Items length: ",this.state.items.length);
+	     // console.log("Show items: ",this.state.showItems);  
   	}
 
 
 	render() {
 
-		let feedDisplay = this.state.items.map((item,i) => {
+		let feedDisplay = this.state.items.slice(0,this.state.showItems).map((item,i) => {
 			const meshUrl = item.meshUrl || this.state.defaultMesh;
 			const name = item.name;
 			const tagElements = item.tags.map((tag,i) => <Grid.Row> <a key={i} className="ui mini tag label">{tag}</a> </Grid.Row>);
@@ -59,11 +71,17 @@ class Feed extends Component {
 	    return (
 		    <Container style={{ marginTop: '7em' }}>
 		    	<Header as='h1'> Mesh Feed </Header>
-			     	<Grid columns={3}>
-			     		<Grid.Row>
-					    	{feedDisplay}
-					    </Grid.Row>
-			  		</Grid>
+	     			<InfiniteScroll
+			          dataLength={this.state.showItems}
+			          next={this.handleShowMore}
+			          hasMore={true}
+			          loader={<h4>Loading...</h4>}
+			        >
+			        	<Grid columns={3}>
+		    				{feedDisplay}
+			    		</Grid>
+			    	</InfiniteScroll>
+			  		
 		    </Container>
 	    );
 	}
